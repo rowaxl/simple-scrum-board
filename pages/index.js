e61fd736a059ed582dadc877a79c6a047cbba6f6
rowaxl <rowaxl0@gmail.com>
2020-06-01 14:49:25 -0700

    add grid container basic layout

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,108 @@
 import Head from '../components/head';
 import Nav from '../components/nav';
 import Background from '../components/background';
-import { Container } from '@material-ui/core'
+import { Container, Grid, Card, CardHeader, CardContent, FormControl, InputLabel, MenuItem, Select, CardActionArea, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    background: 'rgba(255, 255, 255, 0.7)'
+  },
+  taskCards: {
+    minWidth: 300,
+    border: '2px solid #444'
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+  }
+}));
 
-export default () => (
-  <div>
-    <Head title="Simple Scrum Simulator" />
-    <Background />
-    <Nav />
+export default () => {
+  const styles = useStyles();
 
-    <div className="app-wrap">
-      <Container fixed>
-        Hello Next.js
-      </Container>
-    </div>
+  return (
+    <div>
+      <Head title="Simple Scrum Simulator" />
+
+      <Background />
+
+      <Nav />
+
+      <div className="app-wrap">
+        <Container fixed>
+
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="flex-start"
+          >
+
+            <Grid
+              item
+              xs={12}
+              md={4}
+              justify="center"
+            >
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="flex-start"
+                spacing={1}
+              >
+                <Grid item xs={12} md={12}>
+                  <Typography>TODO</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              md={4}
+              justify="center"
+            >
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="flex-start"
+                spacing={1}
+              >
+                <Grid item xs={12} md={12}>
+                  <Typography>DOING</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              md={4}
+              justify="center"
+            >
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="flex-start"
+                spacing={1}
+              >
+                <Grid item xs={12} md={12}>
+                  <Typography>DONE</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+
+          </Grid>
+
+        </Container>
+      </div>
     
 
-    <style jsx>{`
+      <style jsx>{`
       .app-wrap {
         height: 100vh;
         background: rgba(0, 0, 0, 0.3);
@@ -26,5 +111,6 @@ export default () => (
         padding-top: 60px;
       }
     `}</style>
-  </div>
-);
+    </div>
+  )
+};
