39642147031cb15e2d2e9998c132393f91221896
rowaxl <rowaxl0@gmail.com>
2020-06-03 13:47:26 -0700

    update select

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
   },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 200,
+    width: '90%'
   },
 }));
 
@@ -64,7 +64,9 @@ export default () => {
                     </CardContent>
                     <CardActionArea>
                       <FormControl className={styles.formControl}>
-                        <Select defaultValue="">
+                        <InputLabel id="label-select-resource">Resource</InputLabel>
+                        <Select labelId="label-select-resource" defaultValue="" variant="standard">
+                          <MenuItem value="">None</MenuItem>
                           <MenuItem value="p_1">JOHN DOE</MenuItem>
                           <MenuItem value="p_2">JANE DOE</MenuItem>
                           <MenuItem value="p_3">JACK DOE</MenuItem>
